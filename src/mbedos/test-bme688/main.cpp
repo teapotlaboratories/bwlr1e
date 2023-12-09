ba3189ff434e5f64469d21e99403bd90d8288297
Aldwin Hermanudin <aldwin@hermanudin.com>
2023-12-08 18:18:01 -0800

    update simplified bme688 test

--- src/mbedos/test-bme688/main.cpp
+++ src/mbedos/test-bme688/main.cpp
@@ -1,25 +1,37 @@
 #include "mbed.h"
-#include "SensorFusion.hpp"
 #include <stdio.h>
+
+#include "bme688/BME688.h"
 
 static EventQueue mainThread(5 *EVENTS_EVENT_SIZE);
 
+mbed::DigitalOut led0(PA_15);
+mbed::DigitalOut led1(PA_1);
+BME688 iaq_sensor;
 
-DigitalOut hb(PA_15);
-SensorFusion sensorManagement;
-
-
-void doHb();
-
+void checkDataAndPost();
 
 int main()
 {
-    sensorManagement.start();
+    // disable on-board led
+    led0 = 0;
+    led1 = 0;
+
+    iaq_sensor.initialise();
+    iaq_sensor.doMeasurements();
+    mainThread.call_every(15s, checkDataAndPost );
+
     mainThread.dispatch_forever();
 }
- 
-void doHb()
-{
-    hb = !hb;
-}
 
+
+
+void checkDataAndPost()
+{
+    printf("checking if data is available\n\r");
+    if(iaq_sensor.isNewDataAvailable())
+    {
+        printf("data available!\n\r"); 
+        iaq_sensor.dumpData();
+    }
+}
