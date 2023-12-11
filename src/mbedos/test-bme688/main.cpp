fd24d8bfbbb5ad1c70c4f7b7cfc1d1e312a5c064
Aldwin Hermanudin <aldwin@hermanudin.com>
2023-12-10 20:03:27 -0800

    update code

--- src/mbedos/test-bme688/main.cpp
+++ src/mbedos/test-bme688/main.cpp
@@ -1,37 +1,70 @@
+/*
+ * Copyright (c) 2020 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include "mbed.h"
-#include <stdio.h>
 
-#include "bme688/BME688.h"
+Thread thread;
 
-static EventQueue mainThread(5 *EVENTS_EVENT_SIZE);
+mbed::DigitalOut led0(PA_1);
+DigitalOut led1(PA_15);
+volatile bool running = true;
 
-mbed::DigitalOut led0(PA_15);
-mbed::DigitalOut led1(PA_1);
-BME688 iaq_sensor;
-
-void checkDataAndPost();
-
-int main()
+// Blink function toggles the led in a long running loop
+void blink(DigitalOut *led)
 {
-    // disable on-board led
-    led0 = 0;
-    led1 = 0;
-
-    iaq_sensor.initialise();
-    iaq_sensor.doMeasurements();
-    mainThread.call_every(15s, checkDataAndPost );
-
-    mainThread.dispatch_forever();
-}
-
-
-
-void checkDataAndPost()
-{
-    printf("checking if data is available\n\r");
-    if(iaq_sensor.isNewDataAvailable())
-    {
-        printf("data available!\n\r"); 
-        iaq_sensor.dumpData();
+    while (running) {
+        *led = !*led;
+        ThisThread::sleep_for(1000);
     }
 }
+
+// Spawns a thread to run blink for 5 seconds
+int main()
+{
+    led0 = 0;
+    thread.start(callback(blink, &led1));
+    while ( 1 )
+    {
+        ThisThread::sleep_for(5000ms);
+    }
+    // running = false;
+    thread.join();
+}
+
+
+// #include "mbed.h"
+// #include <stdio.h>
+
+// #include "bme688/BME688.h"
+
+// static EventQueue mainThread(5 *EVENTS_EVENT_SIZE);
+
+// mbed::DigitalOut led0(PA_15);
+// mbed::DigitalOut led1(PA_1);
+// BME688 iaq_sensor;
+
+// void checkDataAndPost();
+
+// int main()
+// {
+//     // disable on-board led
+//     led0 = 0;
+//     led1 = 0;
+
+//     iaq_sensor.initialise();
+//     iaq_sensor.doMeasurements();
+//     mainThread.call_every(15s, checkDataAndPost );
+
+//     mainThread.dispatch_forever();
+// }
+
+// void checkDataAndPost()
+// {
+//     printf("checking if data is available\n\r");
+//     if(iaq_sensor.isNewDataAvailable())
+//     {
+//         printf("data available!\n\r"); 
+//         iaq_sensor.dumpData();
+//     }
+// }
--- src/mbedos/test-bme688/readme.md
+++ src/mbedos/test-bme688/readme.md
@@ -1,6 +1,22 @@
+
 # BSEC BME688 Test Example
 
-Mbed Studio need to use GCC_ARM to successfully link BSEC static library
+Tested on:
+
+ - Compiler ARMC6
+   - With/without sleep trace enabled 
+ - [BSEC2 Library](https://www.bosch-sensortec.com/software-tools/software/bme688-software/)
 
 ## Acknowledgement
 Source code is based on https://github.com/Mircerson/AERQ
+
+## TODO
+
+- [ ] Update BSEC library to use the latest version
+- [ ] Provide BSEC static library
+  - `libalgobsec.a` for GCC_ARM
+  - `libalgobsec.ar` for ARMC6 ( currently rename the .lib file to .ar from the BSEC library)
+  - STM32WL55CC is Cortex-M4
+- [ ] Implement simple AQI reader without proper sleep
+- [ ] Rewrite BME688 high-level library
+- [ ] Create a different repo for proper AQI reader with sleep
